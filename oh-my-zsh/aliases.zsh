9f461c4d92bb66deaa8997ec6107b2c131525b80
Will <50718218+evad1n@users.noreply.github.com>
2021-01-06 15:27:50 -0700

    added git prune local untracked alias

--- oh-my-zsh/aliases.zsh
+++ oh-my-zsh/aliases.zsh
@@ -16,6 +16,9 @@ alias mkdir="mkdir -p"
 # gcam exists for lower -a
 alias gcAm="gaa; gcmsg"
 
+# Delete local untracked/deleted branches
+alias git-remove-untracked='git fetch --prune && git branch -r | awk "{print \$1}" | egrep -v -f /dev/fd/0 <(git branch -vv | grep origin) | awk "{print \$1}" | xargs git branch -d'
+
 alias install="sudo apt install"
 
 ###############################################
