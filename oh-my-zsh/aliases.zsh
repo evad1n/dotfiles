f9998df282597b04afd71c747a33817ba701fb84
Will Dickinson <willdickinson98@gmail.com>
2021-02-08 17:04:33 -0700

    updated scal

--- oh-my-zsh/aliases.zsh
+++ oh-my-zsh/aliases.zsh
@@ -1,7 +1,7 @@
 # Editing aliases
 alias etal="$EDITOR $ZSH_CUSTOM/aliases.zsh"
-alias etall="$EDITOR $HOME/aliases-local.zsh"
-alias scal="source $HOME/aliases-local.zsh $ZSH_CUSTOM/aliases.zsh"
+alias etall="$EDITOR $HOME/.aliases-local.zsh"
+alias scal="source $HOME/.aliases-local.zsh $ZSH_CUSTOM/aliases.zsh"
 
 # Ls
 alias l="ls -lh"
