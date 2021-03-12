d34b164ccf72cce7199d424f46440de8be63edd2
Will Dickinson <willdickinson98@gmail.com>
2021-03-12 15:41:37 -0700

    stopped trying to check for installs and just force install now

--- pre-install.sh
+++ pre-install.sh
@@ -1,23 +1,16 @@
 #!/bin/bash
 
-# Check if zsh is installed,
-if [ type zsh &> /dev/null ]
-then
-	echo "zsh not installed. Installing...."
-	sudo apt install zsh
-else
-	echo "zsh already installed"
-fi
+# Install zsh
+echo "Installing zsh..."
+sudo apt install zsh
 
-# Check if oh-my-zsh is installed
+# Install oh-my-zsh
+echo 'Installing oh-my-zsh'
 OMZDIR="$HOME/.oh-my-zsh"
-if [ ! -d "$OMZDIR" ] 
-then
-	echo 'Installing oh-my-zsh'
-	/bin/sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-else
-	echo 'oh-my-zsh already installed'
-fi
+rm -rf  $OMZDIR
+sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+# Remove defualt omz .zshrc
+rm -f $HOME/.zshrc
 
 # OMZ already does this automatically so kkk
 # # Change default shell to zsh
