986bb264166286edcccc4e9f1f636f3f8fde6b91
Will Dickinson <willdickinson98@gmail.com>
2021-04-12 16:02:50 -0600

    tmux/open -> xdg-open

--- oh-my-zsh/aliases.zsh
+++ oh-my-zsh/aliases.zsh
@@ -25,6 +25,7 @@ alias gcAm="gaa; gcmsg"
 alias git-remove-untracked='git fetch --prune && git branch -r | awk "{print \$1}" | egrep -v -f /dev/fd/0 <(git branch -vv | grep origin) | awk "{print \$1}" | xargs git branch -d'
 
 alias install="sudo apt install"
+alias open="xdg-open"
 
 ###############################################
 # Functions (basically more powerful aliases)
