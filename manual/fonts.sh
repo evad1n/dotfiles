c2fb9908377b3152c837f39ec652eb67ef230f6f
Will Dickinson <willdickinson@smartvisionworks.com>
2021-11-17 11:15:00 -0700

    remove sudo from inside fonts script

--- manual/fonts.sh
+++ manual/fonts.sh
@@ -3,13 +3,13 @@
 echo "Installing Meslo LGS Nerd Font..."
 
 # Menlo nerd font
-sudo wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf -O MesloRegular.ttf
-sudo wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold.ttf -O MesloBold.ttf
-sudo wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Italic.ttf -O MesloItalic.ttf
-sudo wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold%20Italic.ttf -O MesloBoldItalic.ttf
+wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf -O MesloRegular.ttf
+wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold.ttf -O MesloBold.ttf
+wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Italic.ttf -O MesloItalic.ttf
+wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold%20Italic.ttf -O MesloBoldItalic.ttf
 
-sudo mkdir /usr/share/fonts/Meslo
-sudo mv *.ttf $_
+mkdir /usr/share/fonts/Meslo
+mv *.ttf $_
 
 # Update font cache
 echo "Updating font cache..."
